1ebb2c2db8a5843753d69bed96b0dc29e02fb03b
ObiDriftKenobi <gdpancake5@gmail.com>
2020-03-19 13:42:15 +0100

    uprava rozlozeni

--- App.js
+++ App.js
@@ -1,10 +1,36 @@
 import * as React from 'react';
-import { Text, View, StyleSheet, ImageBackground, Dimensions, TouchableOpacity } from 'react-native';
+import { useState, useEffect } from 'react';
+import { Text, View, StyleSheet, ImageBackground, Dimensions, TouchableOpacity, Image, Animated, ScrollView } from 'react-native';
 
 var cheerio = require('react-native-cheerio');
 
 // or any pure javascript modules available in npm
 import { Card } from 'react-native-paper';
+
+const FadeInView = (props) => {
+  const [fadeAnim] = useState(new Animated.Value(0))  // Initial value for opacity: 0
+
+  React.useEffect(() => {
+    Animated.timing(
+      fadeAnim,
+      {
+        toValue: 1,
+        duration: 1000,
+      }
+    ).start();
+  }, [])
+
+  return (
+    <Animated.View                 // Special animatable View
+      style={{
+        ...props.style,
+        opacity: fadeAnim,         // Bind opacity to animated value
+      }}
+    >
+      {props.children}
+    </Animated.View>
+  );
+}
 
 class TopBar extends React.Component {
   render() {
@@ -58,26 +84,71 @@ class InfoMenuItem extends React.Component {
 
 class InfoBar extends React.Component {
   state = {
-    view: 0
+    view: 0,
+    opacity: new Animated.Value(0),
+    itemHeight: 0
   }
 
   setPane(pos) {
     this.setState({view: pos})
+    this.animateText()
+  }
+
+  componentDidMount() {
+    this.animateText()
+  }
+
+  animateText() {
+    const reset = this.reset();
+    reset.start(() => {
+      const tim = this.timing();
+      tim.start();
+    });
+    
+  }
+
+  timing() {
+    return Animated.timing(
+      this.state.opacity,
+      {
+        toValue: 1,
+        duration: 500,
+      },
+    )
+  }
+
+  reset() {
+    return Animated.timing(
+      this.state.opacity,
+      {
+        toValue: 0,
+        duration: 1,
+      },
+    )
   }
 
   render() {
     return (
-      <Card style={[styles.cardStyle,{flex: 2, marginRight: 32, height: "100%"}]}>
+      <Card style={[styles.cardStyle,{flex: 2, marginRight: 32}]}>
         <View style={[styles.infoHeader,{flexDirection: "row"}]}>
           <InfoMenuItem title="O co se snažíme" enabled={this.state.view==0} onClick={this.setPane} pos={0} this={this}/>
           <InfoMenuItem title="Hry" enabled={this.state.view==1} onClick={this.setPane} pos={1} this={this}/>
           <InfoMenuItem title="Informace" enabled={this.state.view==2} onClick={this.setPane} pos={2} this={this}/>
         </View>
-        <View style={{flex:1, padding: 32}}>
-          <Header>Lorem ipsum dolor sit</Header>
-          <Paragraph>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Duis risus. Mauris suscipit, ligula sit amet pharetra semper, nibh ante cursus purus, vel sagittis velit mauris vel metus. Phasellus enim erat, vestibulum vel, aliquam a, posuere eu, velit. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Fusce aliquam vestibulum ipsum. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Integer malesuada. Nullam sit amet magna in magna gravida vehicula. Aliquam erat volutpat.</Paragraph>
-          <Paragraph>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Duis risus. Mauris suscipit, ligula sit amet pharetra semper, nibh ante cursus purus, vel sagittis velit mauris vel metus. Phasellus enim erat, vestibulum vel, aliquam a, posuere eu, velit. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Fusce aliquam vestibulum ipsum. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Integer malesuada. Nullam sit amet magna in magna gravida vehicula. Aliquam erat volutpat.</Paragraph>
-        </View>
+        
+        <Animated.View style={{height: "100%", maxHeight: "800px", padding: 32, opacity: this.state.opacity}}>
+          <ScrollView ref={this.scrollview}>
+            { this.state.view==0 &&
+              <View>
+              <Header>Lorem ipsum dolor sit</Header>
+              <Paragraph>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Duis risus. Mauris suscipit, ligula sit amet pharetra semper, nibh ante cursus purus, vel sagittis velit mauris vel metus. Phasellus enim erat, vestibulum vel, aliquam a, posuere eu, velit. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Fusce aliquam vestibulum ipsum. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Integer malesuada. Nullam sit amet magna in magna gravida vehicula. Aliquam erat volutpat.</Paragraph>
+              <Paragraph>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Duis risus. Mauris suscipit, ligula sit amet pharetra semper, nibh ante cursus purus, vel sagittis velit mauris vel metus. Phasellus enim erat, vestibulum vel, aliquam a, posuere eu, velit. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Fusce aliquam vestibulum ipsum. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Integer malesuada. Nullam sit amet magna in magna gravida vehicula. Aliquam erat volutpat.</Paragraph>
+              <Paragraph>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Duis risus. Mauris suscipit, ligula sit amet pharetra semper, nibh ante cursus purus, vel sagittis velit mauris vel metus. Phasellus enim erat, vestibulum vel, aliquam a, posuere eu, velit. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Fusce aliquam vestibulum ipsum. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Integer malesuada. Nullam sit amet magna in magna gravida vehicula. Aliquam erat volutpat.</Paragraph>
+              <Paragraph>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Duis risus. Mauris suscipit, ligula sit amet pharetra semper, nibh ante cursus purus, vel sagittis velit mauris vel metus. Phasellus enim erat, vestibulum vel, aliquam a, posuere eu, velit. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Fusce aliquam vestibulum ipsum. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Integer malesuada. Nullam sit amet magna in magna gravida vehicula. Aliquam erat volutpat.</Paragraph>
+              </View>
+            }
+          </ScrollView>
+        </Animated.View>
       </Card>
     )
   }
@@ -86,17 +157,23 @@ class InfoBar extends React.Component {
 export default class App extends React.Component {
   state = {
     numberOfHealed: 0,
+    mobile: false,
   }
 
   render() {
     return (
       <View style={styles.main} onLayout={this.onResized.bind(this)}>
         <TopBar></TopBar>
-        <ImageBackground source={{uri: require('./assets/grad.png')}} style={styles.pageContent} imageStyle={{resizeMode: 'stretch'}}>
-          <InfoBar />
-          <Card style={[styles.cardStyle,{width: "400px"},{marginLeft: 32}]}>
-            <CardHeader title="Náš koronabot" />
-          </Card>
+        <ImageBackground source={{uri: require('./assets/grad.png')}} imageStyle={{resizeMode: 'stretch'}}>
+          <View style={styles.pageContent}>
+            <InfoBar />
+            <Card style={[styles.cardStyle,{width: "400px", height: "600px"},{marginLeft: 32}]}>
+              <CardHeader title="Náš koronabot" />
+              <ScrollView>
+                
+              </ScrollView>
+            </Card>
+          </View>
         </ImageBackground>
       </View>
     );
@@ -125,6 +202,9 @@ export default class App extends React.Component {
   componentDidMount() {
     //getCovidRecovered()
     this.getCovidRecovered()
+    var ua = navigator.userAgent
+    if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(ua)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(ua.substr(0,4))) 
+    {this.setState({mobile: true})};
   }
 }
 
@@ -152,7 +232,7 @@ const styles = StyleSheet.create({
     paddingVertical: 32,
     paddingTop: 16,
     flex: 1,
-    flexDirection: "row"
+    flexDirection: "row",
   },
   cardStyle: {
     backgroundColor: "rgba(255,255,255,0.5)"
