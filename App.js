114c54ce15495ddcc164aadc6d039fde8310149c
ObiDriftKenobi <gdpancake5@gmail.com>
2020-03-25 17:32:34 +0100

    .

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ import { MaterialIcons } from '@expo/vector-icons';
 var cheerio = require('react-native-cheerio');
 
 // or any pure javascript modules available in npm
-import { Card } from 'react-native-paper';
+import { Card, TouchableRipple } from 'react-native-paper';
 
 const FadeInView = (props) => {
   const [fadeAnim] = useState(new Animated.Value(0))  // Initial value for opacity: 0
@@ -56,7 +56,23 @@ class CardHeader extends React.Component {
 class Header extends React.Component {
   render() {
     return (
-    <Text style={{fontSize: 48, fontWeight: "bold", color: "white"}}>{ this.props.children }</Text>
+    <Text style={{fontSize: 48, fontWeight: 700, fontFamily: "Inter, sans-serif", color: "white", textDecorationLine: 'underline', paddingBottom: 8}}>{ this.props.children }</Text>
+    )
+  }
+}
+
+class News extends React.Component {
+  render() {
+    return (
+        <Card style={{marginVertical: 8}}>
+          <View style={{flexDirection: "row", padding: 8}}>
+            <Image source={{uri: this.props.lnk}} style={{height: 128, width: 128, borderRadius: 4, borderColor: "lightgray", borderWidth: 1}}/>
+            <View style={{flexDirection: "column", marginLeft: 8, flex:1}}>
+              <Text style={{fontSize: 20, fontWeight: 700, fontFamily: "Inter, sans-serif", color: "black", paddingBottom: 8}}>{"„" +  this.props.heading + "“"}</Text>
+              <Text style={{fontSize: 16, color: "black", marginBottom: 16, fontFamily: "Crimson Text, serif"}}>{ this.props.article }</Text>
+            </View>
+          </View>
+        </Card>
     )
   }
 }
@@ -64,7 +80,7 @@ class Header extends React.Component {
 class Paragraph extends React.Component {
   render() {
     return (
-    <Text style={{fontSize: 24, color: "white", marginBottom: 16}}>{ this.props.children }</Text>
+    <Text style={{fontSize: 24, color: "white", marginBottom: 16, fontFamily: "Crimson Text, serif"}}>{ this.props.children }</Text>
     )
   }
 }
@@ -75,7 +91,7 @@ class InfoMenuItem extends React.Component {
       <View style={this.props.enabled ? {borderBottomColor: "#441ECC", borderBottomWidth: 2, flex:1} : {borderBottomColor: "#c4c4c4", borderBottomWidth: 2, flex:1}}>
         <TouchableOpacity onPress={this.props.onClick.bind(this.props.this, this.props.pos)}>
           <View>
-          <Text style={{fontSize: 24, padding: 8, paddingBottom: 6, textAlign: "center"}}>{this.props.title}</Text>
+          <Text style={{fontSize: 24, fontFamily: "Inter, sans-serif", padding: 8, paddingBottom: 6, textAlign: "center"}}>{this.props.title}</Text>
           </View>
         </TouchableOpacity>
       </View>
@@ -88,6 +104,7 @@ class InfoBar extends React.Component {
     view: 0,
     opacity: new Animated.Value(0),
     itemHeight: 0,
+    news: [{"heading":"Koronavirus ustupuje"}]
   }
 
   setPane(pos) {
@@ -100,6 +117,10 @@ class InfoBar extends React.Component {
   componentDidMount() {
     this.animateText()
     this.games = ["covid-shooter"]
+    fetch("https://colon-yrroux.firebaseio.com/news.json").then(response => response.text()).then(response => {
+      response = JSON.parse(response);
+      this.setState({news: response})
+    })
   }
 
   animateText() {
@@ -139,7 +160,7 @@ class InfoBar extends React.Component {
         <TouchableOpacity onPress={() => window.open({"covid-shooter":"https://scratch.mit.edu/projects/377501008/embed"}[item],"_blank")}>
           <Image style={{width:300,height:226,flex:1}} source={{uri: {"covid-shooter":require("./assets/game_icons/covid-shooter.png")}[item]}} />
         </TouchableOpacity>
-      <Text style={{flex:1,textAlign: "center", color: "white", fontSize: 18}}>{{"covid-shooter":"Na virus se zbraní"}[item]}</Text>
+      <Text style={{flex:1,textAlign: "center", color: "white", fontSize: 18, fontFamily: "Inter, sans-serif"}}>{{"covid-shooter":"Na virus se zbraní"}[item]}</Text>
       </View>
     )
   }
@@ -155,7 +176,7 @@ class InfoBar extends React.Component {
     return (
       <Card style={[styles.cardStyle,{flex: 2, marginRight: this.props.margin}]} onLayout={this.props.onLayout}>
         <View style={[styles.infoHeader,{flexDirection: "row"}]}>
-          <InfoMenuItem title="O co se snažíme" enabled={this.state.view==0} onClick={this.setPane} pos={0} this={this}/>
+          <InfoMenuItem title="Zprávy" enabled={this.state.view==0} onClick={this.setPane} pos={0} this={this}/>
           <InfoMenuItem title="Hry" enabled={this.state.view==1} onClick={this.setPane} pos={1} this={this}/>
           <InfoMenuItem title="Informace" enabled={this.state.view==2} onClick={this.setPane} pos={2} this={this}/>
         </View>
@@ -163,23 +184,34 @@ class InfoBar extends React.Component {
         <Animated.View style={{height: "100%", height: "800px", opacity: this.state.opacity}}>
           <ScrollView ref={this.scrollview} style={{padding: 32}}>
             { this.state.view==0 &&
-              <View>
-              <Header>Lorem ipsum dolor sit</Header>
-              <Paragraph>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Duis risus. Mauris suscipit, ligula sit amet pharetra semper, nibh ante cursus purus, vel sagittis velit mauris vel metus. Phasellus enim erat, vestibulum vel, aliquam a, posuere eu, velit. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Fusce aliquam vestibulum ipsum. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Integer malesuada. Nullam sit amet magna in magna gravida vehicula. Aliquam erat volutpat.</Paragraph>
-              <Paragraph>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Duis risus. Mauris suscipit, ligula sit amet pharetra semper, nibh ante cursus purus, vel sagittis velit mauris vel metus. Phasellus enim erat, vestibulum vel, aliquam a, posuere eu, velit. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Fusce aliquam vestibulum ipsum. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Integer malesuada. Nullam sit amet magna in magna gravida vehicula. Aliquam erat volutpat.</Paragraph>
-              <Paragraph>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Duis risus. Mauris suscipit, ligula sit amet pharetra semper, nibh ante cursus purus, vel sagittis velit mauris vel metus. Phasellus enim erat, vestibulum vel, aliquam a, posuere eu, velit. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Fusce aliquam vestibulum ipsum. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Integer malesuada. Nullam sit amet magna in magna gravida vehicula. Aliquam erat volutpat.</Paragraph>
-              <Paragraph>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Duis risus. Mauris suscipit, ligula sit amet pharetra semper, nibh ante cursus purus, vel sagittis velit mauris vel metus. Phasellus enim erat, vestibulum vel, aliquam a, posuere eu, velit. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Fusce aliquam vestibulum ipsum. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Integer malesuada. Nullam sit amet magna in magna gravida vehicula. Aliquam erat volutpat.</Paragraph>
-              </View>
+                <FlatList 
+                data={this.state.news}
+                renderItem={({item, index}) => {
+                  return (
+                    <News heading={item.heading} article={item.article} lnk={item.lnk} />
+                  )
+                }}
+                numColumns={1}
+                style={{width: "100%",flex:1}}
+                listKey="news"
+              />
             }
             { this.state.view==1 &&
               <FlatList 
               data={this.games}
               renderItem={this.gameIcon}
-              numColumns={3}
+              numColumns={this.props.gameMenuCols}
               style={{width: "100%",flex:1}}
               contentContainerStyle={{alignItems: "center"}}
               listKey="games"
+              key={this.props.gameMenuCols}
             />
+            }
+            { this.state.view==2 &&
+            <View>
+              <Header>Lorem ipsum dolor sit</Header>
+              <Paragraph>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Duis risus. Mauris suscipit, ligula sit amet pharetra semper, nibh ante cursus purus, vel sagittis velit mauris vel metus. Phasellus enim erat, vestibulum vel, aliquam a, posuere eu, velit. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Fusce aliquam vestibulum ipsum. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Integer malesuada. Nullam sit amet magna in magna gravida vehicula. Aliquam erat volutpat.</Paragraph>
+            </View>
             }
           </ScrollView>
         </Animated.View>
@@ -195,7 +227,8 @@ export default class App extends React.Component {
     compactMode: false,
     botmessage: "",
     coronaBotMessages: [],
-    isBotWriting: false
+    isBotWriting: false,
+    gameMenuCols: 3
   }
 
   constructor(props) {
@@ -217,19 +250,21 @@ export default class App extends React.Component {
 
           {flexDirection: "row"
           }]}>
-            <InfoBar onLayout={(event) => {this.onResized(event.nativeEvent.layout)}} margin={this.state.mobile || this.state.compactMode ? 0 : 32}/>
+            <InfoBar onLayout={(event) => {this.onResized(event.nativeEvent.layout)}} margin={this.state.mobile || this.state.compactMode ? 0 : 32} gameMenuCols={this.state.gameMenuCols}/>
 
             <Card style={[styles.cardStyle, this.state.mobile || this.state.compactMode ? {height: "600px", marginBottom: 32} : {width: "400px", height: "600px", marginLeft: 32}]}>
               <CardHeader title="Náš koronabot" />
+              <ScrollView style={{maxHeight:"532px", flexDirection: "column-reverse"}}>
               <FlatList
               data={this.state.coronaBotMessages.concat(this.state.isBotWriting ? [{by:"writing"}] : [])}
               renderItem={this.botMessages}
-              style={{width: "100%",flex:1, flexDirection: "column-reverse", padding: 8}}
+              style={{width: "100%", height:"100%", flexDirection: "column-reverse", padding: 8}}
               contentContainerStyle={{}}
               listKey="games"
               />
+              </ScrollView>
               <View style={{flexDirection: "row"}}>
-                <TextInput style={{width: "100%", height: "32px", backgroundColor: "white", borderBottomLeftRadius: "4px", paddingHorizontal: 8}}
+                <TextInput maxLength={256} style={{width: "100%", height: "32px", backgroundColor: "white", borderBottomLeftRadius: "4px", paddingHorizontal: 8, fontFamily: "Inter, sans-serif"}}
                 onSubmitEditing={this.submit} placeholder="Pište zprávu sem..." onChangeText={(text) => this.setState({botmessage:text})} ref={input => { this.textInput = input }}/>
                 <TouchableHighlight onPress={this.submit}>
                   <MaterialIcons name="send" color="#441ECC" size={24} style={{padding: 4, backgroundColor: "white", borderBottomRightRadius: "4px"}}/>
@@ -238,13 +273,13 @@ export default class App extends React.Component {
             </Card>
           </View>
         </ImageBackground>
-        <ImageBackground source={{uri: require('./assets/footer.jpg')}} style={{height: 100, width: "100%", justifyContent: "center"}}>
-        <TouchableOpacity onPress={this.nviasLink}>
+        <ImageBackground imageStyle={{resizeMode: 'stretch'}} source={{uri: require('./assets/footer.jpg')}} style={{height: 100, width: "100%", justifyContent: "center", alignItems: "center"}}>
+        <TouchableHighlight onPress={this.nviasLink} style={{width: 165}}>
           <Image
             style={{width: 165, height: 72, alignSelf: "center"}}
             source={{uri: 'https://www.nvias.org/wp-content/uploads/2017/05/nvias_logo-e1496052667955.png'}}
           />
-        </TouchableOpacity>
+        </TouchableHighlight>
         </ImageBackground>
       </View>
     );
@@ -255,7 +290,15 @@ export default class App extends React.Component {
       if (item.by == "writing") {
         return (
           <View style={{backgroundColor:"#e0e0e0", padding: 8, paddingHorizontal: 12, alignSelf: "flex-start", marginVertical: 1, borderRadius: 20}}>
-              <Text style={{color:"#2d2d2d", fontStyle:"italic"}}>Píše...</Text>
+              <Text style={{color:"#2d2d2d", fontStyle:"italic", fontFamily: "Inter, sans-serif"}}>Píše...</Text>
+            </View>
+        )
+      }
+
+      if (item.by == "error") {
+        return (
+          <View style={{backgroundColor:"red", padding: 8, paddingHorizontal: 12, alignSelf: "flex-start", marginVertical: 1, borderRadius: 20}}>
+              <Text style={{color:"#white", fontFamily: "Inter, sans-serif"}}>Promiňte, nejsem nyní k dispozici. Zkuste to znovu později.</Text>
             </View>
         )
       }
@@ -263,13 +306,13 @@ export default class App extends React.Component {
       if (item.by == "me") {
         return (
           <View style={{backgroundColor:"#441ECC", padding: 8, paddingHorizontal: 12, alignSelf: "flex-end", marginVertical: 1, borderRadius: 20, maxWidth: "80%"}}>
-            <Text style={{color:"white"}}>{item.msg}</Text>
+            <Text style={{color:"white", fontFamily: "Inter, sans-serif"}}>{item.msg}</Text>
           </View>
         )
         } else {
           return (
             <View style={{backgroundColor:"white", padding: 8, paddingHorizontal: 12, alignSelf: "flex-start", marginVertical: 1, borderRadius: 20, maxWidth: "80%"}}>
-              <Text style={{color:"black"}}>{item.msg}</Text>
+              <Text style={{color:"black", fontFamily: "Inter, sans-serif"}}>{item.msg}</Text>
             </View>
           )
         }
@@ -292,7 +335,16 @@ export default class App extends React.Component {
     let msgs = this.state.coronaBotMessages
     msgs.push({by: "me", msg: q})
     this.setState({coronaBotMessages: msgs})
-    this.sleep(700).then(() => {this.setState({isBotWriting: true});})
+    this.sleep(700).then(() => {
+      this.setState({isBotWriting: true});
+      fetch("https://covid19-dobrezpravy--danielsykora.repl.co/dfbot?query=" + encodeURI(q), {method: 'POST'}).then((reply) => reply.text()).then((text) => {this.coronaReply(text)}).catch((error) => {
+        console.error(error);
+        this.setState({isBotWriting: false});
+        let msgs = this.state.coronaBotMessages
+        msgs.push({by: "error"})
+        this.setState({coronaBotMessages: msgs})
+      });
+    })
   }
 
   coronaReply(q) {
@@ -318,29 +370,36 @@ export default class App extends React.Component {
     if (layout.width > 1064 && this.state.compactMode) {
       this.setState({compactMode: false})
     }
+
+    if (layout.width > 1000) {
+      this.setState({gameMenuCols: 3})
+    } else if (layout.width < 1000 && layout.width > 700) {
+      this.setState({gameMenuCols: 2})
+    } else {
+      this.setState({gameMenuCols: 1})
+    }
   }
 
-  getCovidRecovered() {
-    fetch("https://docs.google.com/spreadsheets/d/e/2PACX-1vQuDj0R6K85sdtI8I-Tc7RCx8CnIxKUQue0TCUdrFOKDw9G3JRtGhl64laDd3apApEvIJTdPFJ9fEUL/pubhtml?gid=0&single=true").then(
-        (htmlString) => htmlString.text()).then((response) =>
-        {
-          console.log("getting recovered")
-          var $ = cheerio.load(response);
-          var body = $("body > #sheets-viewport > #0 > div > table > tbody > tr > td.s1");
-          console.log(body.contents().length)
-          var healed = 0
-          for (var i = 0; i < body.contents().length/5; i++) {
-            healed += parseInt(body.eq((i*5)+2).html());
-          }
-          this.setState({numberOfHealed: healed})
-        }
-    )
-}
+  loadRawHtmlModules() {
+    var css = "@import url('https://rsms.me/inter/inter.css');@import url('https://fonts.googleapis.com/css?family=Crimson+Text&display=swap');",
+    head = document.head || document.getElementsByTagName('head')[0],
+    style = document.createElement('style');
+
+    head.appendChild(style);
+
+    style.type = 'text/css';
+    if (style.styleSheet){
+      // This is required for IE8 and below.
+      style.styleSheet.cssText = css;
+    } else {
+      style.appendChild(document.createTextNode(css));
+    }
+  }
 
   componentDidMount() {
     //getCovidRecovered()
-    this.getCovidRecovered()
     var ua = navigator.userAgent
+    this.loadRawHtmlModules();
     if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(ua)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(ua.substr(0,4))) 
     {this.setState({mobile: true})};
   }
@@ -361,8 +420,9 @@ const styles = StyleSheet.create({
     color: "white",
     width: "100%",
     textAlign: "center",
-    fontStyle: "italic",
     fontSize: 48,
+    fontFamily: "Inter, sans-serif",
+    fontWeight: 700
   },
   pageContent: {
     paddingHorizontal: 128,
@@ -377,18 +437,19 @@ const styles = StyleSheet.create({
     width: "100%",
     padding: 8,
     backgroundColor: "white",
-    borderTopLeftRadius: "4px",
-    borderTopRightRadius: "4px",
+    borderTopLeftRadius: 4,
+    borderTopRightRadius: 4,
     justifyContent: "center"
   },
   cardHeaderText: {
     fontSize: 24,
+    fontFamily: "Inter, sans-serif",
   },
   infoHeader: {
     width: "100%",
     backgroundColor: "white",
-    borderTopLeftRadius: "4px",
-    borderTopRightRadius: "4px",
-    justifyContent: "left"
+    borderTopLeftRadius: 4,
+    borderTopRightRadius: 4,
+    justifyContent: "flex-start"
   },
 });
