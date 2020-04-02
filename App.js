74712dc536152a4cbb61de126007fc993adc6181
ObiDriftKenobi <gdpancake5@gmail.com>
2020-04-02 12:53:51 +0200

    Workaround pro prohlizec EDGE kde byl problem s rozbitym chatbotem

--- App.js
+++ App.js
@@ -76,7 +76,7 @@ class CardHeader extends React.Component {
       <View style={styles.cardHeader}>
          <Text style={styles.cardHeaderText}>{this.props.title}</Text>
       </View>
-    )
+    );
   }
 }
 
@@ -84,7 +84,7 @@ class Header extends React.Component {
   render() {
     return (
     <Text style={{fontSize: 48, fontWeight: 700, fontFamily: "Inter, sans-serif", color: "white", textDecorationLine: 'underline', paddingBottom: 8}}>{ this.props.children }</Text>
-    )
+    );
   }
 }
 
@@ -92,7 +92,7 @@ class SubHeader extends React.Component {
   render() {
     return (
     <Text style={{fontSize: 28, fontWeight: 700, fontFamily: "Inter, sans-serif", color: "white", paddingBottom: 8}}>{ this.props.children }</Text>
-    )
+    );
   }
 }
 
@@ -234,7 +234,7 @@ class InfoBar extends React.Component {
             }
             { this.state.view==1&&!this.state.showGame &&
               <View style={[{flex:1},this.props.compactMode ? {margin:0} : {margin:32}]}>
-                <TouchableOpacity onPress={() => {if (!this.props.mobile) {this.setState({showGame: true})} else {window.open("https://scratch.mit.edu/projects/215556802/embed","_blank")}}}>
+                <TouchableOpacity onPress={() => {if (!this.props.mobile) {this.setState({showGame: true});} else {window.open("https://scratch.mit.edu/projects/215556802/embed","_blank");}}}>
                 <Image onLayout={(event) => {this.onResized(event.nativeEvent.layout)}}  style={[{width:"100%",flex:1, alignSelf: "center", marginBottom: 8, backgroundColor: "#005959", borderRadius: 4},this.state.shrinkGameIcon ? {height:300} : {height:640}]} source={{uri: require("./assets/game_icons/covid-shooter.png")}} resizeMode='contain'/>
                 </TouchableOpacity>
               <Text style={{flex:1,textAlign: "center", color: "white", fontSize: 18, fontFamily: "Inter, sans-serif"}}>Na virus se zbraní (Stiskni pro spuštění)</Text>
@@ -288,7 +288,8 @@ export default class App extends React.Component {
     hints: ["Ahoj!"],
     showHints: true,
     isBotWriting: false,
-    gameMenuCols: 3
+    gameMenuCols: 3,
+    keyRender: 0
   }
 
   constructor(props) {
@@ -311,15 +312,15 @@ export default class App extends React.Component {
           }]}>
             <InfoBar compactNews={this.state.compactNews} compactMode={this.state.compactMode} mobile={this.state.mobile} onLayout={(event) => {this.onResized(event.nativeEvent.layout)}} margin={this.state.mobile || this.state.compactMode ? 0 : 32} gameMenuCols={this.state.gameMenuCols}/>
 
-            <Card style={[styles.cardStyle, this.state.mobile || this.state.compactMode ? {height: "600px", marginBottom: 32} : {width: "400px", height: "600px", marginLeft: 32}]}>
+            <Card style={[styles.cardStyle, this.state.mobile || this.state.compactMode ? {marginBottom: 32} : {width: "400px",marginLeft: 32},{height:"600px"}]}>
               <CardHeader title="Náš koronabot" />
-              <ScrollView style={{maxHeight:"532px", flexDirection: "column-reverse"}}>
+              <ScrollView style={[{flexDirection: "column-reverse"},this.state.coronaBotMessages.length > 5 ? {maxHeight:"532px"} : {maxHeight:"534px"}]}>
               <FlatList
               data={this.state.coronaBotMessages.concat(this.state.isBotWriting ? [{by:"writing"}] : [])}
               renderItem={this.botMessages}
               style={{width: "100%", height:"100%", flexDirection: "column-reverse", padding: 8, paddingVertical: 4}}
-              contentContainerStyle={{}}
               listKey="games"
+              extraData={this.state.botMessages}
               />
 
               <View style={{flexDirection: "column", backgroundColor: "white"}}>
@@ -427,13 +428,14 @@ export default class App extends React.Component {
       this.askCoronaBot(this.state.botmessage);
       this.setState({botmessage:"",hints:[],showHints: false});
       this.textInput.clear();
-      this.textInput.getRenderedComponent().focus();
+      this.textInput.current.focus();
     }
   }
 
   askCoronaBot(q) {
     let msgs = this.state.coronaBotMessages
     msgs.push({by: "me", msg: q})
+    console.log(msgs)
     this.setState({coronaBotMessages: msgs})
     this.sleep(700).then(() => {
       this.setState({isBotWriting: true});
@@ -518,8 +520,11 @@ export default class App extends React.Component {
   }
 
   loadRawHtmlModules() {
-    var css = "@import url('https://rsms.me/inter/inter.css');",
-    head = document.head || document.getElementsByTagName('head')[0],
+    var css = "@import url('https://rsms.me/inter/inter.css'); div.r-overflowX-11yh6sk > div.css-view-1dbjc4n > div.css-view-1dbjc4n:empty {display:none;} div.r-11yh6sk > div.css-1dbjc4n > div.css-1dbjc4n:empty {display:none;}";
+    // Tohle importuje písmo z webfontu.
+    // Také se zde nachází workaround pro edge, do kterého se z
+    // neznámého důvodu přidával prázdný blok do chatu. Toto CSS ho skryje.
+    head = document.head || document.getElementsByTagName('head')[0];
     style = document.createElement('style');
 
     head.appendChild(style);
@@ -538,7 +543,8 @@ export default class App extends React.Component {
     var ua = navigator.userAgent
     this.loadRawHtmlModules();
     if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(ua)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(ua.substr(0,4))) 
-    {this.setState({mobile: true})};
+    {this.setState({mobile: true,keyRender:1})};
+    this.setState({keyRender:1});
   }
 }
 
